10aea727ba3195434efb907f71fa3d16f0563fcd
Daniel Chung <danielvchung@hotmail.com>
2019-12-12 14:04:21 -0300

    page registration form template

--- templates/admin/pages/create.tpl.php
+++ templates/admin/pages/create.tpl.php
@@ -1,6 +1,27 @@
 <h3>Administração de páginas.</h3>
-<hr>
-<h4>Criação</h4>
-<hr>
 
-<a href="/admin">Administração</a>
+<form action="" method="POST">
+    <div class="form-group">
+        <!-- Emmet command = label+input.form-control -->
+        <label for="pagesTitle">Título</label>
+        <input id="pagesTitle" class="form-control" name="title" type="text" placeholder="Aqui vai título da página...">
+    </div>
+
+    <div class="form-group">
+        <label for="pagesUrl">URL</label>
+        <div class="input-group">
+            <div class="input-group-prepend">
+                <span class="input-group-text">/</span>
+            </div>
+            <input id="pagesUrl" class="form-control" name="url" type="text" placeholder="URL amigável, deixe em branco para informar página inicial...">
+        </div>
+    </div>
+
+    <div class="form-group">
+        <!-- editor de texto -->
+    </div>
+    <!-- Emmet command = button:submit.btn.btn-primary -->
+    <button type="submit" class="btn btn-primary">Salvar</button><hr>
+</form>
+
+<a class="btn btn-secondary" href="/admin">Voltar</a>
--- templates/admin/pages/index.tpl.php
+++ templates/admin/pages/index.tpl.php
@@ -1,6 +1,24 @@
-<h3>Administração de páginas.</h3>
-<hr>
-<h4>Listagem</h4>
-<hr>
+<h3 class="mb-5">Administração de páginas.</h3>
 
-<a href="/admin">Administração</a>
+<!-- Emmet command = table.table.table-hover>thead>tr>th*^^tbody>tr>td*3 -->
+<table class="table table-hover">
+    <thead>
+        <tr>
+            <th>#</th>
+            <th>Título</th>
+            <th></th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td><a href="/admin/pages/1">Página inicial</a></td>
+            <td class="text-right">
+                <!-- Emmet command = a.btn.btn-primary.btn-sm -->
+                <a href="/admin/pages/1" class="btn btn-primary btn-sm">ver</a>
+            </td>
+        </tr>
+    </tbody>
+</table> <!-- end Emmet command -->
+
+<a class="btn btn-secondary" href="/admin/pages/create">Novo</a>
