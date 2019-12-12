02475cfa64089e29f213a0fe1d3203560e55a545
Daniel Chung <danielvchung@hotmail.com>
2019-12-12 14:29:24 -0300

    adding trix editor library

---
+++ README.md
@@ -0,0 +1,6 @@
+# Bibliotecas usadas:
+
+- Bootstrap 4 (https://getbootstrap.com/)
+- jQuery (https://jquery.com/)
+- Trix Editor (https://github.com/basecamp/trix). Ir no diretório *dist/* e copiar arquivos *trix.css* e *trix.js*
+
--- templates/admin/pages/create.tpl.php
+++ templates/admin/pages/create.tpl.php
@@ -4,7 +4,7 @@
     <div class="form-group">
         <!-- Emmet command = label+input.form-control -->
         <label for="pagesTitle">Título</label>
-        <input id="pagesTitle" class="form-control" name="title" type="text" placeholder="Aqui vai título da página...">
+        <input id="pagesTitle" class="form-control" name="title" type="text" placeholder="Aqui vai título da página..." required>
     </div>
 
     <div class="form-group">
@@ -18,7 +18,9 @@
     </div>
 
     <div class="form-group">
-        <!-- editor de texto -->
+        <!-- Usado biblioteca Trix. Emmet command = trix-editor -->
+        <input id="pagesBody" name="body" type="hidden" required>
+        <trix-editor input="pagesBody"></trix-editor>
     </div>
     <!-- Emmet command = button:submit.btn.btn-primary -->
     <button type="submit" class="btn btn-primary">Salvar</button><hr>
